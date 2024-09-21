3d3f91496a17ce7b7ddc461f857f1aa0ce15baf9
Inigo Zulueta <inigoz@csu.fullerton.edu>
2024-09-20 20:39:27 -0700

    Finished (mostly) with the remove whitespace function. Working on counting keyword lexemes and keyword tokens next.

--- main.py
+++ main.py
@@ -6,21 +6,29 @@ class MissingKeywordError(Exception):
 def remove_whitespace(input_file, output_file):
     # Read the input file. In this case, it's a text file.
     with open(input_file, 'r') as f:
+        # Side comment: If i need to read lines for a large database or smth, it's best that I create a hint parameter
+        # just so I can avoid crashing my pc. Thank you for not making this overkill.
         lines = f.readlines()
 
     # Empty set that will hold each line in the text file as an element of the set.
     processed_lines = []
     # Returns a copy of a string with whitespaces removed.
     for line in lines:
-        line == line.strip()
-    if line:
-        # Remove any additional spaces after using strip(), then use a separator to split and discard the
-        # rest of the whitespaces. We re-add a space using the join() and append() function to concacenate the strings 
-        # with extra spaces removed. What we get is no more whitespace. Each line becomes an element of the processed
-        # lines set (Line 12).
-        spaces = line.split()
-        line = ' '.join(spaces)
-        processed_lines.append(line)
+        if '#' in line:
+            line = line[:line.index('#')]
+            line = line.strip()
+        if line:
+
+        # Remove any additional comments using strip(), then use some sort of separator (in this case it's split()) 
+        # to split and discard the rest of the whitespaces. We re-add a space using the join() and append() function to
+        # concacenate the substrings with extra spaces removed.
+
+        # What we get is no more whitespace. Each line becomes an element of the processed lines set (Line 12).
+        # We add a string containing "space" in order to recombine the rest of the substrings.
+        
+            spaces = line.split()
+            line = ' '.join(spaces)
+            processed_lines.append(line)
 
     # We then open a new file and make it writable. Since each element of the set is a line, we write each element to 
     # the new file and separate each element with a \n. This gives us a new text file with no whitespaces and we are
@@ -30,4 +38,12 @@ def remove_whitespace(input_file, output_file):
             f.write(line + '\n')
 
 def check_keywords(output_file):
-    pass
+    
+    # Create a dictionary of keywords. Since this is a small project I can get away with only listing keywords used
+    # for the example code.
+    reserved_keywords = {
+        'def',
+        'if',
+        'print',
+    }
+    #TODO: Print the lexemes and count the number of keyword tokens.
