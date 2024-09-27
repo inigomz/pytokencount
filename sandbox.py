eb025318f7a26196cf8b190f48f04c93792accd9
Inigo Zulueta <inigoz@csu.fullerton.edu>
2024-09-26 17:02:26 -0700

    Used sandbox.py to test out functions for catching strings that have keywords. This will be implemented in the main function folder. TODO: Finish the function folder, create a new main.py foler, then use the python file as some sort of library.

---
+++ sandbox.py
@@ -0,0 +1,18 @@
+# Testing with dictionaries and key-value pairs
+keyword_category = {
+'def' : 0,
+'if' : 0,
+'print' : 0,
+}
+
+# Testing for incramentals
+text_file = "def if print owl rain ( ) , : 2 1 3"
+defcheck = 'def'
+def test_for_incramentals(keyword_category, text_file, defcheck):
+    if defcheck in text_file:
+        keyword_category['def'] += 1
+    return keyword_category
+
+# main test program
+test_for_incramentals(keyword_category, text_file, defcheck)
+print(keyword_category)
