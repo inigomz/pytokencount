f48cc6f41fecd6a13eeb6800f5ce6f8bd5e3bdf3
Inigo Zulueta <inigoz@csu.fullerton.edu>
2024-10-09 01:12:11 -0700

    Saving so I can port everything to windows for final revisions

--- functions.py
+++ functions.py
@@ -1,3 +1,5 @@
+# Keeping this in here in the event I want to call upon different files other than input_file.txt or output_file.txt.
+# Technically this MissingKeywordError will not be used.
 class MissingKeywordError(Exception):
     def __init__(self, message):
         self.message = message
@@ -75,10 +77,12 @@ reserved_litterals = {
 
 # Main idea behind the check functions is to make everything as reusable as possible.
 
+# Pretty much: open file (hence process file) > check to see if token exists in the file using a dictionary >
+# If token exists in the file, add 1 value to its key : value in the dictionary > return the dictionary.
+
 # All of the check functions do the same thing with ONE exception: 
 # They call upon different dictionaries with different key: value pairs.
 def check_keywords(reserved_keywords, output_file):
-    #TODO: find a way to initialize the output file so it sets each word as a string.
     with open(output_file, 'r') as f:
         processed_file = f.read() 
         for keyword in reserved_keywords:
@@ -87,39 +91,61 @@ def check_keywords(reserved_keywords, output_file):
         print('Keywords have been successfuly processed')
         return reserved_keywords
     
-def check_identifiers (reserved_identifiers, output_file):
-    for identifier in reserved_identifiers:
-        if identifier in output_file:
-            reserved_identifiers[identifier] += 1
-        else:
-            print('Identifiers have successfully been processed')
-            return reserved_identifiers
-def check_operators (reserved_operators, output_file):
-    for operator in reserved_operators:
-        if operator in output_file:
-            reserved_operators[operator] += 1
-        else:
-            print('Operators have successfully been processed')
-            return reserved_operators
-def check_delimiters (reserved_delimiters, output_file):
-    for delimiter in reserved_delimiters:
-        if delimiter in output_file:
-            reserved_delimiters[delimiter] += 1
-        else:
-            print('Delimiters have successfully been processed')
-            return reserved_delimiters
-def check_litterals (reserved_litterals, output_file):
-    for litteral in reserved_litterals:
-        if litteral in output_file:
-            reserved_litterals[litteral] += 1
-        else:
-            print('Litterals have successfully been processed')
-            return reserved_litterals
+def check_identifiers(reserved_identifiers, output_file):
+    with open(output_file, 'r') as f:
+        processed_file = f.read() 
+        for identifier in reserved_identifiers:
+            if identifier in processed_file:
+                reserved_identifiers[identifier] += 1
+        print('Identifiers have successfully been processed')
+        return reserved_identifiers
     
-    #TODO: Print the lexemes and count the number of keyword tokens.
+def check_operators(reserved_operators, output_file):
+    with open(output_file, 'r') as f:
+        processed_file = f.read() 
+        for operator in reserved_operators:
+            if operator in processed_file:
+                reserved_operators[operator] += 1
+        print('Operators have successfully been processed')
+        return reserved_operators
+    
+def check_delimiters(reserved_delimiters, output_file):
+    with open(output_file, 'r') as f:
+        processed_file = f.read() 
+        for delimiter in reserved_delimiters:
+            if delimiter in processed_file:
+                reserved_delimiters[delimiter] += 1
+        print('Delimiters have successfully been processed')
+        return reserved_delimiters
+        
+def check_litterals(reserved_litterals, output_file):
+    with open(output_file, 'r') as f:
+        processed_file = f.read() 
+        for litteral in reserved_litterals:
+            if litteral in processed_file:
+                reserved_litterals[litteral] += 1
+        print('Litterals have successfully been processed')
+        return reserved_litterals
 
-def show_lexemes ():
-    print(reserved_keywords)
+# This is what shows the table. Pretty self explanitory. Combine all dictionaries into one
+# while making sure everything looks pretty.
+def show_lexemes():
     all_lexemes = reserved_keywords | reserved_identifiers | reserved_delimiters | reserved_litterals | reserved_operators
-    print(all_lexemes)
-    print(sum(all_lexemes.values()))
+    # Nobody has time for formatting and making things look pretty 
+    # (and I dont wanna guestimate with f strings).
+
+    #Sets the width
+    width = 15
+    
+    # Print header
+    print(f"{'Lexeme'.center(width)} | {'Count'.center(width)}")
+    print('-' * (width * 2 + 3))
+    
+    # Print each lexeme and its count
+    for lexeme, count in all_lexemes.items():
+        print(f"{lexeme.center(width)} | {str(count).center(width)}")
+    
+    # Print the total sum of lexemes
+    total_lexemes = sum(all_lexemes.values())
+    print('-' * (width * 2 + 3))
+    print(f"{'Total'.center(width)} | {str(total_lexemes).center(width)}")
